bb8f428137f309228429f875d10e824a35021ef5
utkuufuk <utkuufuk@gmail.com>
2019-01-08 20:55:46 +0300

    infer start & end hours from input file automatically

--- weekplot.py
+++ weekplot.py
@@ -3,14 +3,8 @@ from math import ceil
 import matplotlib.pyplot as plt
 from namedlist import namedlist
 
-HOURS_START = 6.5
-HOURS_END = 24
-HOURS_RANGE = range(ceil(HOURS_START), ceil(HOURS_END))
-DPI = 200
-TITLE = 'Schedule'
 DAYS = ['Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-Event = namedlist('Event', 'name, days, start, end, color')
+Event = namedlist('Event', 'name, days, startH, startM, endH, endM, color')
 
 def getDay(prefix):
     for d in DAYS:
@@ -19,8 +13,10 @@ def getDay(prefix):
     raise UserWarning("Invalid day: {0}".format(prefix))
 
 def parseEvents(lines):
-    events = [Event('', '', '', '', '')]
     index = 0
+    latest = 0
+    earliest = 24
+    events = [Event('', '', '', '', '', '', '')]
     for line in lines:
         line = line.rstrip()
         index += 1
@@ -30,46 +26,51 @@ def parseEvents(lines):
             events[-1].days = [getDay(d) for d in line.replace(' ', '').split(',')]
         elif index == 3:
             hours = line.replace(' ', '').split('-')
-            events[-1].start = hours[0]
-            events[-1].end = hours[1]
+            start = hours[0].split(':')
+            end = hours[1].split(':')
+            events[-1].startH = int(start[0])
+            events[-1].startM = int(start[1])
+            events[-1].endH = int(end[0])
+            events[-1].endM = int(end[1])
+            earliest = events[-1].startH if events[-1].startH < earliest else earliest
+            latest = events[-1].endH + 1 if events[-1].endH > latest else latest
         elif index == 4:
             events[-1].color = line
         elif index == 5 and line == '':
-            events.append(Event('', '', '', '', ''))
+            events.append(Event('', '', '', '', '', '', ''))
             index = 0
         else:
             raise UserWarning("Corrupted input file.")
-    return events
+    return events, earliest, latest
 
-def plotEvent(event):
-    for day in event.days:
+def plotEvent(e):
+    for day in e.days:
         d = DAYS.index(day) + 0.52
-        start = event.start.split(':')
-        startPos = float(start[0]) + float(start[1]) / 60
-        end = event.end.split(':')
-        endPos = float(end[0]) + float(end[1]) / 60
-        plt.fill_between([d, d + 0.96], [startPos, startPos], [endPos, endPos], color=event.color)
-        plt.text(d + 0.02, startPos + 0.02, '{0}:{1:0>2}'.format(start[0], start[1]), va='top', fontsize=8)
-        plt.text(d + 0.48, (startPos + endPos) * 0.502, event.name, ha='center', va='center', fontsize=10)
+        start = float(e.startH) + float(e.startM) / 60
+        end = float(e.endH) + float(e.endM) / 60
+        plt.fill_between([d, d + 0.96], [start, start], [end, end], color=e.color)
+        plt.text(d + 0.02, start + 0.02, '{0}:{1:0>2}'.format(e.startH, e.startM), va='top', fontsize=8)
+        plt.text(d + 0.48, (start + end) * 0.502, e.name, ha='center', va='center', fontsize=10)
 
 if __name__ == '__main__':
     fig = plt.figure(figsize=(18, 9))
-    plt.title(TITLE, y=1, fontsize=14)
-    ax=fig.add_subplot(1, 1, 1)
-    ax.set_xlim(0.5, len(DAYS) + 0.5)
-    ax.set_xticks(range(1, len(DAYS) + 1))
-    ax.set_xticklabels(DAYS)
-    ax.set_ylim(HOURS_END, HOURS_START)
-    ax.set_yticks(HOURS_RANGE)
-    ax.set_yticklabels(["{0}:00".format(h) for h in HOURS_RANGE])
-    ax.grid(axis='y', linestyle='--', linewidth=0.5)
     with open(sys.argv[1]) as fp:
         lines = fp.readlines()
     try:
-        events = parseEvents(lines)
+        events, earliest, latest = parseEvents(lines)
         for e in events:
             plotEvent(e)
     except UserWarning as e:
         print("ERROR:", str(e), file=sys.stderr)
         sys.exit(1)
-    plt.savefig('{0}.png'.format(sys.argv[1].split('.')[0]), dpi=DPI, bbox_inches='tight')
+    plt.title('Weekly Schedule', y=1, fontsize=14)
+    ax=fig.add_subplot(1, 1, 1)
+    ax.set_xlim(0.5, len(DAYS) + 0.5)
+    ax.set_xticks(range(1, len(DAYS) + 1))
+    ax.set_xticklabels(DAYS)
+    ax.set_ylim(latest, earliest)
+    ax.set_yticks(range(ceil(earliest), ceil(latest)))
+    ax.set_yticklabels(["{0}:00".format(h) for h in range(ceil(earliest), ceil(latest))])
+    ax.grid(axis='y', linestyle='--', linewidth=0.5)
+    plt.savefig('{0}.png'.format(sys.argv[1].split('.')[0]), dpi=200, bbox_inches='tight')
+
